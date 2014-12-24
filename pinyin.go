33eae3cd0013bfcf5b1ef8d973ea97ce0e87e200
Miclle <miclle.zheng@gmail.com>
2014-12-24 21:59:33 +0800

    Update dict, pinyin package

--- pinyin.go
+++ pinyin.go
@@ -1,42 +1,51 @@
-package main
+package pinyin
 
 import (
   "strings"
-  // "regexp"
+  "regexp"
 )
 
-// var hzRegexp = regexp.MustCompile("^[\u4e00-\u9fa5]$")
-
-var dict = map[string]string{}
+var hzRegexp = regexp.MustCompile("^[\u4e00-\u9fa5]$")
 
 // Translate chinese to pinyin
 func T(args ...string) string {
-  splitter, argsLen := " ", len(args)
 
-  var fslice = []string{}
+  splitter, argsLen := " ", len(args)
 
   if argsLen == 0 {
     return ""
   }
+
+  var str string
+  var non = ""
+  var fslice = []string{}
 
   if argsLen > 1 {
     splitter = args[argsLen - 1]
   }
 
   for _, rune := range args[0] {
-    fslice = append(fslice, string(rune))
+
+    str = string(rune)
+
+    if str == " " {
+      continue
+    }
+
+    if hzRegexp.MatchString(str) { //chinese
+      if non != ""{
+        fslice = append(fslice, non)
+        non = ""
+      }
+      fslice = append(fslice, strings.Split(PinyinDict[str], ",")[0])
+    }else{
+      non += str
+    }
+  }
+
+  if non != ""{
+    fslice = append(fslice, non)
   }
 
   return strings.Join(fslice, splitter)
-}
-
-
-func init() {
-
-  for _, line := range strings.Split(dictDate, "\n") {
-
-    var words = strings.SplitN(line, "\t", 2)
-
-    dict[words[0]] = words[1]
-  }
 }
