448df1b143d7a360340346875ad7d00ac7a9a14d
Miclle <miclle.zheng@gmail.com>
2014-12-24 22:06:28 +0800

    Add cli tool.

---
+++ pinyin/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+  "os"
+  "strings"
+  "pinyin"
+)
+
+func main() {
+  str, splitter := "", " "
+
+  if len(os.Args) == 2 {
+    str = os.Args[1]
+  }
+
+  if len(os.Args) > 2 {
+    str = strings.Join(os.Args[1:len(os.Args)-1], "")
+    splitter = os.Args[len(os.Args)-1]
+  }
+
+  println(pinyin.T(str, splitter))
+}
