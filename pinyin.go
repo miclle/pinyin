03cdd69af389d627af6db8e7a28d9b3551936726
Miclle <miclle.zheng@gmail.com>
2015-01-05 23:01:48 +0800

    go fmt

--- pinyin.go
+++ pinyin.go
@@ -1,93 +1,88 @@
 package pinyin
 
 import (
-  "strings"
-  "regexp"
-  "unicode/utf8"
+	"regexp"
+	"strings"
+	"unicode/utf8"
 )
-
 
 var hzRegexp = regexp.MustCompile("^[\u4e00-\u9fa5]$")
 
-
 // Translate chinese to pinyin
 func T(args ...string) string {
-  return translate(args, false)
+	return translate(args, false)
 }
-
 
 func TT(args ...string) string {
-  return translate(args, true)
+	return translate(args, true)
 }
-
 
 func translate(args []string, tone bool) string {
 
-  splitter, argsLen := " ", len(args)
+	splitter, argsLen := " ", len(args)
 
-  if argsLen == 0 {
-    return ""
-  }
+	if argsLen == 0 {
+		return ""
+	}
 
-  var str string
-  var non = ""
-  var fslice = []string{}
+	var str string
+	var non = ""
+	var fslice = []string{}
 
-  if argsLen > 1 {
-    splitter = args[argsLen - 1]
-  }
+	if argsLen > 1 {
+		splitter = args[argsLen-1]
+	}
 
-  for _, rune := range args[0] {
+	for _, rune := range args[0] {
 
-    str = string(rune)
+		str = string(rune)
 
-    if str == " " {
-      continue
-    }
+		if str == " " {
+			continue
+		}
 
-    if hzRegexp.MatchString(str) { //chinese
-      if non != ""{
-        fslice = append(fslice, non)
-        non = ""
-      }
+		if hzRegexp.MatchString(str) { //chinese
+			if non != "" {
+				fslice = append(fslice, non)
+				non = ""
+			}
 
-      fslice = append(fslice, spell(str, tone))
+			fslice = append(fslice, spell(str, tone))
 
-    }else{
-      non += str
-    }
-  }
+		} else {
+			non += str
+		}
+	}
 
-  if non != ""{
-    fslice = append(fslice, non)
-  }
+	if non != "" {
+		fslice = append(fslice, non)
+	}
 
-  return strings.Join(fslice, splitter)
+	return strings.Join(fslice, splitter)
 }
-
 
 func spell(str string, tone bool) string {
 
-  spell := strings.Split(PinyinDict[str], ",")[0]
+	spell := strings.Split(PinyinDict[str], ",")[0]
 
-  if tone {
-    return spell
-  }
+	if tone {
+		return spell
+	}
 
-  output := make([]string, utf8.RuneCountInString(spell))
+	output := make([]string, utf8.RuneCountInString(spell))
 
-  count := 0
+	count := 0
 
-  for _, tone := range spell {
-    neutral, found := tones[string(tone)]
-    if found {
-      output[count] = neutral
-    } else {
-      output[count] = string(tone)
-    }
-    count++
-  }
+	for _, tone := range spell {
+		neutral, found := tones[string(tone)]
+		if found {
+			output[count] = neutral
+		} else {
+			output[count] = string(tone)
+		}
+		count++
+	}
 
-  return strings.Join(output, "")
+	return strings.Join(output, "")
 
 }
